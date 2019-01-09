b0a9c097f78db21d178cade0e492f922d3b46d37
rbrown3 <rosannabrown@outlook.com>
2019-01-09 12:04:25 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,15 @@ class Song
     @@count
   end
   def self.artists
-    artists_array = []
-    artists_array << self.artist
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+      @@genres.inject(Hash.new(0)) { |total,e| total[e] += 1 ;total}
+  end
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
   end
 end
